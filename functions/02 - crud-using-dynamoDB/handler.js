c28f63a336938359362a0079ebed25e91da6d0bb
Leonardo do Nascimento dos Santos <leonardo.nascimento@marche.com.br>
2021-07-17 19:14:16 -0300

    create a crud using dynamoDB

--- functions/02 - crud-using-dynamoDB/handler.js
+++ functions/02 - crud-using-dynamoDB/handler.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const awsSdk = require('aws-sdk')
+const { v4: uuid} = require('uuid')
 
 module.exports.listCustomers = async () => {
 
@@ -27,17 +28,31 @@ module.exports.listCustomers = async () => {
 };
 
 module.exports.saveCustomers = async event => {
+
   try {
     const parserBody = JSON.parse(event.body)
     
     const { name, age, gender, email, phone } = parserBody
+
+    const customer = {
+      id: uuid(),
+      name,
+      age,
+      gender,
+      email,
+      phone,
+      created_at: new Date(),
+      updated_at: new Date(),
+    }
   
     return {
       statusCode: 200,
-      body: event.body
+      body: JSON.stringify(customer)
     }
 
   } catch (error) {
+
+    console.log(error);
     return {
       statusCode: 500,
       body: JSON.stringify(error)
